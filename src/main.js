e9e21ce46fd62a9cdbb0e6fb271f77c9b052aec4
davydocsurg <davydocsurg@gmail.com>
2021-09-05 01:06:57 +0200

    add: redirect onLoad

--- src/main.js
+++ src/main.js
@@ -1,23 +1,27 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 // import router from './router'
-import store from './store'
+import store from "./store";
 
-import '../src/assets/css/main.css'
-import '../public/assets/fontawesome-free/css/all.css'
-import Scroll from './animations/scroll'
+import "../src/assets/css/main.css";
+import "../public/assets/fontawesome-free/css/all.css";
+import Scroll from "./animations/scroll";
 // import nav from './nav'
 
-import 'bootstrap/dist/css/bootstrap.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.directive('scrollAnime', Scroll)
+Vue.directive("scrollAnime", Scroll);
+
+document
+  .getElementById("app")
+  .onload(window.location.replace("https://davydocsurg.github.io"));
 
 new Vue({
   // router,
   store,
 
   // nav,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
